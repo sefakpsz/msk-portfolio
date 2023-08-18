69798cb9f346ae48cf4c3158ea97eef8695582a6
sefa kapısız <sefa.kapisiz@coino.com>
2023-08-18 19:14:40 +0300

    process

--- components/Main/MainItem.tsx
+++ components/Main/MainItem.tsx
@@ -1,6 +1,14 @@
-const MainItem = () => {
+interface MainItemProps {
+    title: string
+}
+
+const MainItem: React.FC<MainItemProps> = ({ title }) => {
     return (
-        <div>MainItem</div>
+        <li className='mb-10 ml-6'>
+            <span className="absolute flex items-center justify-center w-2.5 h-2.5 bg-purple-400 rounded-full -left-[5px] ring-[10px] ring-gray-500 ring-opacity-25"></span>
+            <div className='relative ml-4 -top-2 text-lg font-light'>{title}</div>
+            <VerticalList />
+        </li>
     )
 }
 
